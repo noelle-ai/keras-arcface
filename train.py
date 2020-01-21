f4d015b4a60d6e9a63cea70b06ffd7fb50441d7a
noelle-ai <noelleland@naver.com>
2020-01-21 14:54:38 +0900

    noeun

--- train.py
+++ train.py
@@ -1,36 +1,32 @@
-import time
+# [ Load Package ]
+# ----------------------
 import os
-import math
 import argparse
-from glob import glob
-from collections import OrderedDict
-import random
-import warnings
-from datetime import datetime
 import joblib
-
 import numpy as np
-import pandas as pd
-
-from sklearn.model_selection import KFold, StratifiedKFold
-
 import keras
 from keras.datasets import mnist
-from keras.preprocessing.image import ImageDataGenerator
-from keras.models import Sequential, load_model
-from keras.layers import Dense, Dropout, Activation, Flatten
-from keras.layers import Conv2D, MaxPooling2D
 from keras.optimizers import SGD, Adam
-from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau, CSVLogger, LearningRateScheduler, TerminateOnNaN, LambdaCallback
-
+from keras.callbacks import ModelCheckpoint, CSVLogger,  TerminateOnNaN
 import archs
 from metrics import *
 from scheduler import *
+# ----------------------
 
+# [ GPU Setting ]
+# ----------------------
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+config = tf.ConfigProto()
+config.gpu_options.per_process_gpu_memory_fraction = 0.4
+sessopm = tf.Session(config=config)
+# ----------------------
 
+# vgg8_arcface
+arch_names = archs.__dict__.keys()
 arch_names = archs.__dict__.keys()
 
-
+# Options
 def parse_args():
     parser = argparse.ArgumentParser()
 
@@ -52,6 +48,7 @@ def parse_args():
                         help='number of total epochs to run')
     parser.add_argument('-b', '--batch-size', default=128, type=int,
                         metavar='N', help='mini-batch size (default: 128)')
+    # ! Adam method : Adagrad + RMSPro Cost Func
     parser.add_argument('--optimizer', default='SGD',
                         choices=['Adam', 'SGD'],
                         help='loss: ' +
@@ -75,17 +72,20 @@ def main():
 
     os.makedirs('models/%s' %args.name, exist_ok=True)
 
+    # print options
     print('Config -----')
     for arg in vars(args):
         print('%s: %s' %(arg, getattr(args, arg)))
     print('------------')
 
-    joblib.dump(args, 'models/%s/args.pkl' %args.name)
+    joblib.dump(args, 'models/%s/args.pkl' %args.name) # save model as pickled binary file
 
     with open('models/%s/args.txt' %args.name, 'w') as f:
         for arg in vars(args):
             print('%s: %s' %(arg, getattr(args, arg)), file=f)
 
+    # [ Target Data Generating ]
+    # ---------------------- MNIST
     (X, y), (X_test, y_test) = mnist.load_data()
 
     X = X[:, :, :, np.newaxis].astype('float32') / 255
@@ -93,17 +93,24 @@ def main():
 
     y = keras.utils.to_categorical(y, 10)
     y_test = keras.utils.to_categorical(y_test, 10)
+    # ----------------------
 
+    # [ Optimizer Setting ]
+    # ----------------------
     if args.optimizer == 'SGD':
         optimizer = SGD(lr=args.lr, momentum=args.momentum)
     elif args.optimizer == 'Adam':
         optimizer = Adam(lr=args.lr)
+    # ----------------------
 
+    # [ Model Setting ]
+    # ----------------------
     model = archs.__dict__[args.arch](args)
     model.compile(loss='categorical_crossentropy',
             optimizer=optimizer,
             metrics=['accuracy'])
-    model.summary()
+    model.summary() # check
+    # ----------------------
 
     callbacks = [
         ModelCheckpoint(os.path.join('models', args.name, 'model.hdf5'),
@@ -114,8 +121,9 @@ def main():
     if args.scheduler == 'CosineAnnealing':
         callbacks.append(CosineAnnealingScheduler(T_max=args.epochs, eta_max=args.lr, eta_min=args.min_lr, verbose=1))
 
+    # [ Train ]
+    # ----------------------
     if 'face' in args.arch:
-        # callbacks.append(LambdaCallback(on_batch_end=lambda batch, logs: print('W has nan value!!') if np.sum(np.isnan(model.layers[-4].get_weights()[0])) > 0 else 0))
         model.fit([X, y], y, validation_data=([X_test, y_test], y_test),
             batch_size=args.batch_size,
             epochs=args.epochs,
@@ -127,16 +135,19 @@ def main():
             epochs=args.epochs,
             callbacks=callbacks,
             verbose=1)
+    # ----------------------
 
+    # [ Model Evaluate ]
+    # ----------------------
     model.load_weights(os.path.join('models/%s/model.hdf5' %args.name))
     if 'face' in args.arch:
         score = model.evaluate([X_test, y_test], y_test, verbose=1)
     else:
         score = model.evaluate(X_test, y_test, verbose=1)
+    # ----------------------
 
     print("Test loss:", score[0])
     print("Test accuracy:", score[1])
-
 
 if __name__ == '__main__':
     main()
